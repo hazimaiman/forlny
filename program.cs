734493175eccf611fe71144336261089ca8b4850
hazimaiman <79648084+hazimaiman@users.noreply.github.com>
2023-01-05 13:23:56 +0800

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,31 @@
+
+namespace MyFirstProgram
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            /*
+            for (int i= 0; i < 10; i++)
+
+            {
+                Console.WriteLine(i);
+            }
+            */
+            for( int i = 10;i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Happy new years");
+
+
+            Console.ReadKey();
+            }
+        }
+    }
+
+
+
+
+
+  
